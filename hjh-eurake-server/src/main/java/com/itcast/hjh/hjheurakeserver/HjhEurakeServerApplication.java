1d53fceff6f66562c34baaefc85ed9b79b41afbf
hanjinghui <892427642@qq.com>
2020-07-07 16:08:01 +0800

    首次提交

---
+++ hjh-eurake-server/src/main/java/com/itcast/hjh/hjheurakeserver/HjhEurakeServerApplication.java
@@ -0,0 +1,13 @@
+package com.itcast.hjh.hjheurakeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HjhEurakeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HjhEurakeServerApplication.class, args);
+    }
+
+}
---
+++ hjh-eurake-server/src/test/java/com/itcast/hjh/hjheurakeserver/HjhEurakeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itcast.hjh.hjheurakeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HjhEurakeServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
